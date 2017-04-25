712759a1877cc4b022f8c40b1190b97d3d7e55a4
computesta <computesta@gmail.com>
2017-04-25 17:34:07 +0700

    Update and rename HelloController.php to CronController.php
    Mengubah HelloController menjadi CronController

--- commands/CronController.php
+++ commands/CronController.php
@@ -10,14 +10,12 @@ namespace app\commands;
 use yii\console\Controller;
 
 /**
- * This command echoes the first argument that you have entered.
- *
- * This command is provided as an example for you to learn how to create console commands.
+ * Aplikasi khusus untuk menjalankan cron job umum.
  *
  * @author Qiang Xue <qiang.xue@gmail.com>
  * @since 2.0
  */
-class HelloController extends Controller
+class CronController extends Controller
 {
     /**
      * This command echoes what you have entered as the message.
